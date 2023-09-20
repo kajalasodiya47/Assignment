2a0dd88dcdbbec34447d8cc747702cc8888ab7d0
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2023-09-20 14:09:38 +0530

    Update reverse_string_palindrome_or_not.c

--- module_3_c_language/reverse_string_palindrome_or_not.c
+++ module_3_c_language/reverse_string_palindrome_or_not.c
@@ -1,10 +1,20 @@
-#include<stdio.h>
+#include<stdio.h>              // header file
 #include<string.h>
-int main()
+int main()                     // main() function with int return type
 {
-	char name[100],i;
+	char name[100],reversename[100];     // declare variables
 	printf("Enter your string:");
 	scanf("%s",&name);
-	strrev(name);
-	printf("Reverse of a string is:%s",name);
+	strcpy(reversename,name);           // copy name string
+	strrev(reversename);                // reverse string
+	printf("Reverse of a string is:%s",reversename);
+	if(strcmp(name,reversename) == 0)           // compare reversename string with name string
+	{
+		printf("\n%s is a palindrome",name);
+	}
+	else
+	{
+		printf("\n%s is not a palindrome",name);
+	}
+	return 0;                           // return 0 ends the main() function
 }
